b323bc474d7341550f48bd236bae915cf7f5c0d3
Pagarelov <vladimir.pagarelov2022@gmail.com>
2023-12-23 14:46:08 +0300

    added SpaceShip_01

---
+++ Assets/Scripts/Destructible.cs
@@ -0,0 +1,68 @@
+using UnityEngine;
+
+namespace SpaceShooter
+{
+    /// <summary>
+    /// Destroyed object on stage. Something that can have hit points.
+    /// </summary>
+    public class Destructible : Entity
+    {
+        #region Properties
+
+        /// <summary>
+        /// Player ignores damage.
+        /// </summary>
+        [SerializeField] private bool m_Indestructible;
+        public bool IsIndestructible => m_Indestructible;
+
+        /// <summary>
+        /// Starting number of hit points.
+        /// </summary>
+        [SerializeField] private int m_HitPoints;
+
+        /// <summary>
+        /// Current hit points.
+        /// </summary>
+        private int m_CurrentHitPoints;
+        public int HitPoints => m_CurrentHitPoints;
+
+        #endregion
+
+
+        #region Unity Events
+
+        protected virtual void Start()
+        {
+            m_CurrentHitPoints = m_HitPoints;
+        }
+
+        #endregion
+
+
+        #region Public API
+        /// <summary>
+        /// Applying damage to an object.
+        /// </summary>
+        /// <param name="damage">Damage done to an object</param>
+        public void ApplyDamage(int damage)
+        {
+            if (m_Indestructible) return;
+
+            m_CurrentHitPoints -= damage;
+
+            if (m_CurrentHitPoints <= 0)
+                OnDeath();
+        }
+
+        #endregion
+
+        /// <summary>
+        /// Overrideable object destruction event when hitpoints are below zero.
+        /// </summary>
+        protected virtual void OnDeath()
+        {
+            Destroy(gameObject);
+        }
+    }
+
+}
---
+++ Assets/Scripts/SpaceShip.cs
@@ -0,0 +1,88 @@
+using UnityEngine;
+
+namespace SpaceShooter
+{
+    [RequireComponent(typeof(Rigidbody2D))]
+    public class SpaceShip : Destructible
+    {
+        /// <summary>
+        /// Weight for automatic rigid installation.
+        /// </summary>
+        [Header("Space ship")]
+        [SerializeField] private float m_Mass;
+
+        /// <summary>
+        /// Pushing force forward.
+        /// </summary>
+        [SerializeField] protected float m_Thrust;
+
+        /// <summary>
+        /// Rotating force.
+        /// </summary>
+        [SerializeField] protected float m_Mobility;
+
+        /// <summary>
+        /// Maximum linear speed.
+        /// </summary>
+        [SerializeField] protected float m_MaxLinearVelocity;
+
+        /// <summary>
+        /// Maximum rotational speed.In degrees/sec.
+        /// </summary>
+        [SerializeField] protected float m_MaxAngularVelocity;
+
+        /// <summary>
+        /// Saved link to rigid.
+        /// </summary>
+        private Rigidbody2D m_Rigid;
+
+        #region Public API
+
+        /// <summary>
+        /// Linear traction control. -1.0 to +1.0.
+        /// </summary>
+        public float ThrustControl { get; set; }
+
+        /// <summary>
+        /// Rotational thrust control. -1.0 to +1.0.
+        /// </summary>
+        public float TorqueControl { get; set; }
+
+        #endregion
+
+        #region Ubity Event
+
+        protected override void Start()
+        {
+            base.Start();
+
+            m_Rigid = GetComponent<Rigidbody2D>();
+            m_Rigid.mass = m_Mass;
+
+            m_Rigid.inertia = 1;
+        }
+
+        private void FixedUpdate()
+        {
+            UpdateRigidbody();
+        }
+
+        #endregion
+
+        /// <summary>
+        /// Method of adding forces to a ship for movement.
+        /// </summary>
+        private void UpdateRigidbody()
+        {
+            m_Rigid.AddForce(ThrustControl * m_Thrust * transform.up * Time.fixedDeltaTime, ForceMode2D.Force);
+
+            m_Rigid.AddForce(-m_Rigid.velocity * (m_Thrust / m_MaxLinearVelocity) * Time.fixedDeltaTime, ForceMode2D.Force);
+
+            m_Rigid.AddTorque(TorqueControl * m_Mobility * Time.fixedDeltaTime, ForceMode2D.Force);
+
+            m_Rigid.AddTorque(-m_Rigid.angularVelocity * (m_Mobility / m_MaxAngularVelocity) * Time.fixedDeltaTime, ForceMode2D.Force);
+        }
+
+    }
+}
+
