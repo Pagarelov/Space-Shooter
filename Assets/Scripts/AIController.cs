baae37f5f9df74896290509aa631fa487196f409
Pagarelov <vladimir.pagarelov2022@gmail.com>
2024-01-13 17:59:38 +0300

    added ship ai, ai can move

---
+++ Assets/Scripts/AIController.cs
@@ -0,0 +1,123 @@
+using System;
+using UnityEngine;
+
+namespace SpaceShooter
+{
+    [RequireComponent(typeof(SpaceShip))]
+    public class AIController : MonoBehaviour
+    {
+        public enum AIBehaviour
+        {
+            Null,
+            Patrol
+        }
+
+        [SerializeField] private AIBehaviour m_AIBehaviour;
+
+        [Range(0.0f, 1.0f)]
+        [SerializeField] private float m_NavigationLinear;
+
+        [Range(0.0f, 1.0f)]
+        [SerializeField] private float m_NavigationAngular;
+
+        [SerializeField] private float m_RandomSelectMovePointTime;
+
+        [SerializeField] private float m_FindNewTargetTime;
+
+        [SerializeField] private float m_ShootDelay;
+
+        [SerializeField] private float m_EvadeRayLength;
+
+        private SpaceShip m_SpaceShip;
+
+        private Vector3 m_MovePosition;
+
+        private Destructible m_SelectedTarget;
+
+        private Timer TestTimer;
+
+        private const float MAX_ANGLE = 45.0f;
+
+        private void Start()
+        {
+            m_SpaceShip = GetComponent<SpaceShip>();
+
+            InitTimers();
+        }
+
+        private void Update()
+        {
+            UpdateTimers();
+
+            UpdateAI();
+        }
+
+        private void UpdateAI()
+        {
+            if (m_AIBehaviour == AIBehaviour.Patrol)
+            {
+                UpdateBehaviourPatrol();
+            }
+        }
+
+        private void UpdateBehaviourPatrol()
+        {
+            ActionFindNewMovePosition();
+            ActionControlShip();
+            ActionFindNewAttackTarget();
+            ActionFire();
+        }
+
+        private void ActionFindNewMovePosition()
+        {
+            
+        }
+
+        private void ActionControlShip()
+        {
+            m_SpaceShip.ThrustControl = m_NavigationLinear;
+
+            m_SpaceShip.TorqueControl = ComputeAlignTorqueNormalized(m_MovePosition , m_SpaceShip.transform) * m_NavigationAngular;
+        }
+
+        private static float ComputeAlignTorqueNormalized(Vector3 targetPosition, Transform ship)
+        {
+            Vector2 localTargetPosition = ship.InverseTransformPoint(targetPosition);
+
+            float angle = Vector3.SignedAngle(localTargetPosition, Vector3.up, Vector3.forward);
+
+            Debug.Log(angle);
+
+            angle = Mathf.Clamp(angle, -MAX_ANGLE, MAX_ANGLE) / MAX_ANGLE;
+
+            return -angle;
+        }
+
+        private void ActionFindNewAttackTarget()
+        {
+            
+        }
+
+        private void ActionFire()
+        {
+            
+        }
+
+
+
+        #region Timers
+
+        private void InitTimers()
+        {
+
+        }
+
+        private void UpdateTimers()
+        {
+
+        }
+
+        #endregion
+    }
+}
+
